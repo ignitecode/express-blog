1177f1342fb5ff60555ea253608cc5f9df9409d1
Christian <cbartram3@gmail.com>
2019-04-05 09:12:58 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Use middleware logging & POST body parsing
+app.use(morgan('combined'));
+app.use(bodyParser({ extended: true }));
+
+// Import our controller code
+const blogController = require('./controllers/blogController');
+
+// Setup our Views
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// Link our Controllers
+app.use('/blogs', blogController);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(chalk.blue(`[INFO] Server Listening on Port: ${port}`));
+});
